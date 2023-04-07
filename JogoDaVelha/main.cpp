0197cca210b157941f62bbc337c6ad8e935eb782
André Figueiredo <andre.figueiredo@miniclip.com>
2023-04-07 01:14:21 +0100

    Initial Commit

---
+++ JogoDaVelha/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  JogoDaVelha
+//
+//  Created by André Figueiredo on 07/04/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
